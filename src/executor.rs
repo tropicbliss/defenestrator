985b1e19464989164aed918f57e65468bd67175b
tropicbliss <tropicbliss@protonmail.com>
2021-10-29 00:59:59 +0800

    E

--- src/executor.rs
+++ src/executor.rs
@@ -1,6 +1,7 @@
 use std::time::Instant;
 
 use anyhow::Result;
+use console::style;
 use futures::{stream, StreamExt};
 use reqwest::Client;
 use std::io::{stdout, Write};
@@ -12,21 +13,21 @@ pub async fn run(names: Vec<String>, parallel_requests: usize) -> Result<Vec<Str
     let bodies: Vec<_> = stream::iter(names)
         .map(|name| {
             let url = format!("https://api.ashcon.app/mojang/v2/user/{}", name);
-            let client = client.clone();
-            tokio::spawn(async move {
+            let client = &client;
+            async move {
                 let resp = client.get(&url).send().await.expect("Got a reqwest::Error");
                 match resp.status().as_u16() {
                     200 => {
-                        println!("{} was taken", name);
+                        println!("{} was taken", style(name).yellow());
                         NameResult::Taken
                     }
                     404 => {
-                        println!("{} is available", name);
+                        println!("{} is available", style(&name).yellow());
                         NameResult::Available(name)
                     }
                     _ => panic!("HTTP {}", resp.status()),
                 }
-            })
+            }
         })
         // Limiting concurrency to prevent OS from running out of resources
         .buffer_unordered(parallel_requests)
@@ -36,14 +37,16 @@ pub async fn run(names: Vec<String>, parallel_requests: usize) -> Result<Vec<Str
     writeln!(stdout())?;
     writeln!(
         stdout(),
-        "{:.11} rqs/sec (ESTIMATE) | Took {} seconds | {} requests",
-        (name_list_len as f64 / elapsed.as_secs_f64()),
+        "{:.11} {} | {} {} {} | {} requests",
+        style(name_list_len as f64 / elapsed.as_secs_f64()).green(),
+        style("rqs/sec (ESTIMATE)").cyan(),
+        style("Took").cyan(),
         elapsed.as_secs_f32(),
+        style("seconds").cyan(),
         name_list_len
     )?;
     let mut available_names = Vec::new();
     for body in bodies {
-        let body = body?;
         if let NameResult::Available(name) = body {
             available_names.push(name);
         }
