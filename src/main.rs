ac88491db71b8d09da5c5f1c7e35290451f47c59
tropicbliss <tropicbliss@protonmail.com>
2021-11-17 14:54:12 +0800

    E

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,13 @@ async fn main() -> Result<()> {
     let names = utils::get_names(&args.input)
         .with_context(|| format!("Failed to get names from {}", args.input.display()))?;
     let name_validity_data = utils::get_name_validity(names);
-    let available_names = executor::run(name_validity_data.valid_names, args.parallel_requests, args.timeout)
-        .await
-        .with_context(|| "Failed to run executor")?;
+    let available_names = executor::run(
+        name_validity_data.valid_names,
+        args.parallel_requests,
+        args.timeout,
+    )
+    .await
+    .with_context(|| "Failed to run executor")?;
     writeln!(stdout())?;
     writeln!(stdout(), "Available username(s): {:?}", available_names)?;
     if !name_validity_data.invalid_names.is_empty() {
