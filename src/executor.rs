af74fc884d5688d042e364a2d5eff6242d16d12a
tropicbliss <tropicbliss@protonmail.com>
2022-02-12 21:27:01 +0800

    Add support for new mojang api endpoint

--- src/executor.rs
+++ src/executor.rs
@@ -30,48 +30,43 @@ pub async fn run(names: Vec<String>, parallel_requests: usize, delay: u64) -> Re
             }
         }
     });
-    let bodies: Vec<_> = stream::iter(
-        names
-            .chunks(10)
-            .map(Into::into)
-            .collect::<Vec<Vec<String>>>(),
-    )
-    .map(|name| {
-        // Client has its own internal Arc impl so each clone is just cloning a reference to it
-        let client = client.clone();
-        let tx = tx.clone();
-        tokio::spawn(async move {
-            loop {
-                let json = json!(name);
-                let resp = client
-                    .post("https://api.mojang.com/profiles/minecraft")
-                    .json(&json)
-                    .send()
-                    .await
-                    .expect("Error while sending request");
-                match resp.status().as_u16() {
-                    200 => {
-                        let result: Vec<Unit> = resp.json().await.unwrap();
-                        let result: Vec<String> =
-                            result.into_iter().map(|unit| unit.name).collect();
-                        for name in &result {
-                            println!("{} was taken", Yellow.paint(name));
+    let bodies: Vec<_> = stream::iter(names.chunks(10).map(Into::into).collect::<Vec<Vec<_>>>())
+        .map(|name| {
+            // Client has its own internal Arc impl so each clone is just cloning a reference to it
+            let client = client.clone();
+            let tx = tx.clone();
+            tokio::spawn(async move {
+                loop {
+                    let json = json!(name);
+                    let resp = client
+                        .post("https://api.mojang.com/profiles/minecraft")
+                        .json(&json)
+                        .send()
+                        .await
+                        .expect("Error while sending request");
+                    match resp.status().as_u16() {
+                        200 => {
+                            let result: Vec<Unit> = resp.json().await.unwrap();
+                            let result: Vec<String> =
+                                result.into_iter().map(|unit| unit.name).collect();
+                            for name in &result {
+                                println!("{} was taken", Yellow.paint(name));
+                            }
+                            break result;
                         }
-                        break result;
+                        429 => {
+                            tx.send(MsgState::Locking).unwrap();
+                            sleep(Duration::from_secs(delay)).await;
+                        }
+                        _ => panic!("HTTP {}", resp.status()),
                     }
-                    429 => {
-                        tx.send(MsgState::Locking).unwrap();
-                        sleep(Duration::from_secs(delay)).await;
-                    }
-                    _ => panic!("HTTP {}", resp.status()),
                 }
-            }
+            })
         })
-    })
-    // Limiting concurrency to prevent OS from running out of resources
-    .buffer_unordered(parallel_requests)
-    .collect()
-    .await;
+        // Limiting concurrency to prevent OS from running out of resources
+        .buffer_unordered(parallel_requests)
+        .collect()
+        .await;
     tx.send(MsgState::Exit)?;
     let mut available_names = Vec::new();
     for body in bodies {
